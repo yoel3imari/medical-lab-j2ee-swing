d50474b635f8f879e17506c9c3e15a8b80b684a3
yoel3imari <youssef.elaimari@gmail.com>
2024-04-14 23:11:49 +0100

    mvc pattern

--- src/main/java/com/mycompany/medicallab/MedicalLab.java
+++ src/main/java/com/mycompany/medicallab/MedicalLab.java
@@ -1,16 +1,16 @@
 package com.mycompany.medicallab;
 
 import com.mycompany.medicallab.views.LoginFrame;
+import java.sql.Connection;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
-/**
- *
- * @author yusef
- */
 public class MedicalLab {
 
     public static void main(String[] args) {
         // init db connection
-        Database.connection();
+
+        Connection db = Database.getInstance().getConnection();
         // start login frame
         LoginFrame login = new LoginFrame();
         // redirect to dashboard
