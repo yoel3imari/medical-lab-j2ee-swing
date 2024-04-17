2d6897936417511b0d2c3f35d311f1de5593851a
ElyazidAmhaoune <elyazidamhaoune@gmail.com>
2024-04-18 00:48:14 +0100

    Add JPA annotations to Test class

--- src/main/java/com/mycompany/medicallab/models/Test.java
+++ src/main/java/com/mycompany/medicallab/models/Test.java
@@ -4,14 +4,56 @@
  */
 package com.mycompany.medicallab.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 /**
  *
  * @author yusef
  */
-//@Entity 
+@Entity
 public class Test {
-    private int id;
-    private String lable;
-    private Double price;
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String label;
+    private double price;
+
+    // Constructors, getters, and setters
+    public Test() {
+    }
+
+    public Test(String label, double price) {
+        this.label = label;
+        this.price = price;
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
+
