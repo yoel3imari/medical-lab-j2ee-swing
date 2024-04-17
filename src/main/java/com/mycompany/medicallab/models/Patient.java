4a346f25b745f4ea94e6da585a32b1d359636601
badrhrm <badr.eddine.hrimech@proton.me>
2024-04-17 21:23:16 +0100

    patient model

--- src/main/java/com/mycompany/medicallab/models/Patient.java
+++ src/main/java/com/mycompany/medicallab/models/Patient.java
@@ -1,13 +1,117 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.medicallab.models;
 
-/**
- *
- * @author yusef
- */
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.Date;
+
+
+@Entity
+@Table(name = "patient")
 public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, unique = true)
+    private Long id;
+
+    @Column(name = "fName", length = 25)
+    private String fName;
+
+    @Column(name = "lName", length = 25)
+    private String lName;
+
+    @Column(name = "cin", length = 12)
+    private String cin;
+
+    @Column(name = "email", length = 50)
+    private String email;
+
+    @Column(name = "phone", length = 10)
+    private String phone;
+
+    @Column(name = "gender", length = 6)
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Column(name = "birthdate")
+    private Date birthdate;
+
+    // Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getCin() {
+        return cin;
+    }
+
+    public void setCin(String cin) {
+        this.cin = cin;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
     
+    public enum Gender {
+        Male,
+        Female
+    }
 }
+
+
+
